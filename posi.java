f9ba2e4dbff8bde4f1c811bcfe91ee83dbbe9273
swathiganapathy <swathiganapathy96@gmail.com>
2017-07-15 12:37:59 +0530

    Create posi.java

---
+++ posi.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class posi
+{
+    public static void main(String args[])
+    {
+        Scanner ob=new Scanner(System.in);
+        int a=ob.nextInt();
+        if(a>=0)
+        {
+            System.out.print("positive");
+        }
+        else if(a<0)
+        System.out.print("negative");
+}
